cfd84ea1b71a5359c9c355c0656bfe8910b70790
Hemense Lan <hlan@afexnigeria.com>
2024-09-08 19:31:43 +0100

    setup mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import '@mantine/core/styles.css';
+
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "routes";
 
+import { MantineProvider } from '@mantine/core';
+
 function App() {
   return (
     <React.Suspense>
-      <RouterProvider router={router} />
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </React.Suspense>
   );
 }
