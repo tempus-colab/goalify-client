cd6c194303358a31ed21a823f5343742f6d86bc4
Hemense Lan <hlan@afexnigeria.com>
2024-09-22 12:07:50 +0100

    chore: add react query for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,26 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "routes";
 
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const [client] = React.useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {},
+        mutations: {},
+      },
+    }),
+  );
+
   return (
     <React.Suspense>
       <MantineProvider withCssVariables>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={client}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </MantineProvider>
     </React.Suspense>
   );
